0610a40d0013c25c2c94d9d5cb872f99c7044bef
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-10-20 19:08:35 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Container from './components/container';
+//import Section from './components/feedback/section/Section';
+import Feedback from './components/feedback';
+//import FeedbackOptions from './components/feedback/feedbackOptions/FeedbackOptions';
+//import Statistics from './components/feedback/statistics/Statistics';
+//import Notification from './components/feedback/notification/Notification';
+
+function App() {
+  return (
+    <Container>
+      <Feedback />
+    </Container>
+  );
+}
+
+export default App;
